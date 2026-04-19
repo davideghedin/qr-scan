e75ef6558172a414def0f2ab759b4e67c6762fdb
davideghedin <davide.ghedin@gmail.com>
2026-04-19 21:54:01 +0200

    Update google.js

--- google.js
+++ google.js
@@ -1,4 +1,5 @@
-
+const FORM_ID = "1FAIpQLSeV-QY4T_a2E07ChO0i4emFPICWW8vhhB2T0j5hZGsRa6y8JA";
+const ENTRY_ID = "entry.622568697"; 
 
 function inviaAGoogleForms(valoreQR) {
     const url = `https://docs.google.com/forms/d/e/${FORM_ID}/formResponse`;
