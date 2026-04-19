6fdb99b82b8c28e810eaab2a1bf6c9a9c1ad4567
davideghedin <davide.ghedin@gmail.com>
2026-04-19 22:45:58 +0200

    Update google.js

--- google.js
+++ google.js
@@ -1,5 +1,6 @@
 const FORM_ID = "1FAIpQLSeV-QY4T_a2E07ChO0i4emFPICWW8vhhB2T0j5hZGsRa6y8JA";
 const ENTRY_ID = "entry.622568697"; 
+const ENTRY_ID_2 = "entry.1021393218"; 
 
 function inviaAGoogleForms(valoreQR) {
     const url = `https://docs.google.com/forms/d/e/${FORM_ID}/formResponse`;
@@ -16,6 +17,22 @@ function inviaAGoogleForms(valoreQR) {
     document.getElementById("status").innerText =
       "QR letto e inviato correttamente ✅";
 }    
+
+function postToGoogleSheet2(name) {
+    const url = `https://docs.google.com/forms/d/e/${FORM_ID}/formResponse`;
+
+    const formData = new FormData();
+    formData.append(ENTRY_ID_2, name);
+
+    fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      body: formData
+    });
+
+    document.getElementById("status").innerText =
+      "QR letto e inviato correttamente ✅";
+}
 
 const webAppUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRehVbW704F9njWYTtCosZSCsVCorlgevGbm8wsSOU8Z_7kK-1a8MUKFHwcPOxtqpxmipaGj71_r07M/pub?output=csv"; // Incolla qui l'URL di Google
 
