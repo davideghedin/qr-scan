428dd1849ac7292af35073d64bcc9e8f0ac5458c
davideghedin <davide.ghedin@gmail.com>
2026-04-19 22:05:13 +0200

    Update google.js

--- google.js
+++ google.js
@@ -17,21 +17,22 @@ function inviaAGoogleForms(valoreQR) {
       "QR letto e inviato correttamente ✅";
 }    
 
-const webAppUrl = "URL_DELLA_TUA_WEB_APP_QUI"; // Incolla qui l'URL di Google
+const webAppUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRehVbW704F9njWYTtCosZSCsVCorlgevGbm8wsSOU8Z_7kK-1a8MUKFHwcPOxtqpxmipaGj71_r07M/pubhtml?gid=1156890236&amp;single=true&amp;widget=true&amp;headers=false"; // Incolla qui l'URL di Google
 
 function getLastRecord() {
 
-fetch(webAppUrl)
-    .then(res => res.json())
-    .then(data => {
-        if (data.error) {
-            document.getElementById('output').innerText = data.error;
-        } else {
-            // Unisce i valori delle colonne con un separatore
-            document.getElementById('output').innerText = data.join(" | ");
-        }
-    })
-    .catch(err => {
-        console.error(err);
-        document.getElementById('output').innerText = "Errore di connessione";
-    });
+    fetch(webAppUrl)
+        .then(res => res.json())
+        .then(data => {
+            if (data.error) {
+                document.getElementById('output').innerText = data.error;
+            } else {
+                // Unisce i valori delle colonne con un separatore
+                document.getElementById('output').innerText = data.join(" | ");
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            document.getElementById('output').innerText = "Errore di connessione";
+        });
+}
